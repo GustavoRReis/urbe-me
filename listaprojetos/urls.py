dd7a75b91656a4e008975d5956307bcedc811840
gustavo ramon dos reis <gustavobilireis@hotmail.com>
2023-05-01 22:04:59 -0300

    feat: cria renderização dos cards e layout

--- listaprojetos/urls.py
+++ listaprojetos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from listaprojetos.views import index
+from listaprojetos.views import index, lista_projetos_view
 
 urlpatterns = [
     path("", index),
+    path("lista-projetos/", lista_projetos_view),
 ]
--- listaprojetos/views.py
+++ listaprojetos/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
-    return render(request, 'homepage.html')
+    return render(request, 'listaprojetos/homepage.html')
 
+
+def lista_projetos_view(request):
+    url = 'https://urbe.me/administracao/api/lista-projetos/'
+    
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+    except Exception as error:
+        print("Erro na chamada da API:", error)
+    context = {'projetos': data}
+    return render(request, 'listaprojetos/homepage.html', context)
